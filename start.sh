e1caf387ff14cdf0deed080d96babecb57c8eb2f
curcuz <czkzcz.cmc@gmail.com>
2016-01-26 23:45:28 +0100

    ensure there are no locks on X11 before starting

--- start.sh
+++ start.sh
@@ -5,4 +5,9 @@
 # the syntax below starts an X istance with ONLY our electronJS fired up,
 # it saves you a LOT of resources avoiding full-desktops envs
 
-startx /app/node_modules/electron-prebuilt/dist/electron /app
+while true
+	do
+    rm /tmp/.X0-lock || true
+		startx /app/node_modules/electron-prebuilt/dist/electron /app --enable-logging
+	done
+fi
