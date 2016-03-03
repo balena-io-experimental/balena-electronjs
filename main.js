8a92b04a9e4726eaf962c4b7de6371bea506f0c2
curcuz <czkzcz.cmc@gmail.com>
2016-03-03 22:19:48 +0100

    fix missing iife

--- main.js
+++ main.js
@@ -1,49 +1,52 @@
-// since we control the whole environment, we can safely use ES6 syntax:
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+(function() {
+  'use strict';
+  // since we control the whole environment, we can safely use ES6 syntax:
+  const electron = require('electron');
+  const app = electron.app;
+  const BrowserWindow = electron.BrowserWindow;
 
-// simple parameters initialization
-let window = null;
-let resin_toolbar = false;
-let resin_kiosk = false;
-let resin_node = true;
+  // simple parameters initialization
+  let window = null;
+  let resin_toolbar = false;
+  let resin_kiosk = false;
+  let resin_node = true;
 
-/*
-  we initialize our application display as a callback of the electronJS "ready" event
-*/
-app.on('ready', function() {
+  /*
+    we initialize our application display as a callback of the electronJS "ready" event
+  */
+  app.on('ready', function() {
 
-    if (process.env.URL_LAUNCHER_FRAME && process.env.URL_LAUNCHER_FRAME === "false") {
-      resin_toolbar = false;
-    }
-    if (process.env.URL_LAUNCHER_KIOSK && process.env.URL_LAUNCHER_KIOSK === "true") {
-      resin_kiosk = true;
-    }
-    if (process.env.URL_LAUNCHER_NODE && process.env.URL_LAUNCHER_NODE === "false") {
-      resin_node = false;
-    }
-
-    // here we actually configure the behavour of electronJS
-    window = new BrowserWindow({
-      width: parseInt(process.env.URL_LAUNCHER_WIDTH || 1920),
-      height: parseInt(process.env.URL_LAUNCHER_HEIGHT || 1080),
-      frame: resin_toolbar,
-      title: process.env.URL_LAUNCHER_TITLE || "RESIN.IO",
-      kiosk: resin_kiosk,
-      webPreferences: {
-        experimentalFeatures: true,
-        overlayScrollbars: true,
-        overlayFullscreenVideo: true,
-        nodeIntegration: resin_node
+      if (process.env.URL_LAUNCHER_FRAME && process.env.URL_LAUNCHER_FRAME === "false") {
+        resin_toolbar = false;
       }
-    });
+      if (process.env.URL_LAUNCHER_KIOSK && process.env.URL_LAUNCHER_KIOSK === "true") {
+        resin_kiosk = true;
+      }
+      if (process.env.URL_LAUNCHER_NODE && process.env.URL_LAUNCHER_NODE === "false") {
+        resin_node = false;
+      }
 
-    // if the env-var is set to true, a portion of the screen will be dedicated to the chrome-dev-tools
-    if (process.env.URL_LAUNCHER_CONSOLE && process.env.URL_LAUNCHER_CONSOLE === "true") {
-      window.openDevTools();
-    }
+      // here we actually configure the behavour of electronJS
+      window = new BrowserWindow({
+        width: parseInt(process.env.URL_LAUNCHER_WIDTH || 1920),
+        height: parseInt(process.env.URL_LAUNCHER_HEIGHT || 1080),
+        frame: resin_toolbar,
+        title: process.env.URL_LAUNCHER_TITLE || "RESIN.IO",
+        kiosk: resin_kiosk,
+        webPreferences: {
+          experimentalFeatures: true,
+          overlayScrollbars: true,
+          overlayFullscreenVideo: true,
+          nodeIntegration: resin_node
+        }
+      });
 
-    // the big red button, here we go
-    window.loadURL(process.env.URL_LAUNCHER_URL || "http://resin.io");
-});
+      // if the env-var is set to true, a portion of the screen will be dedicated to the chrome-dev-tools
+      if (process.env.URL_LAUNCHER_CONSOLE && process.env.URL_LAUNCHER_CONSOLE === "true") {
+        window.openDevTools();
+      }
+
+      // the big red button, here we go
+      window.loadURL(process.env.URL_LAUNCHER_URL || "http://resin.io");
+  });
+})();
