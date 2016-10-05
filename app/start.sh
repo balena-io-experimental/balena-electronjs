c1c1ba62b5bd47cd2779f8bc6a2b12447e9c1ae3
Rahat Ahmed <rahatarmanahmed@gmail.com>
2016-10-04 22:41:21 -0500

    Use electron instead of electron-prebuilt
    `electron-prebuilt` is deprecated and will no longer receive updates after 2016. `electron` is the recommended package name to use.

--- app/start.sh
+++ app/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# using electron-prebuilt module instead of the global electron let's you
+# using local electron module instead of the global electron lets you
 # easily control specific version dependency between your app and electron itself.
 # the syntax below starts an X istance with ONLY our electronJS fired up,
 # it saves you a LOT of resources avoiding full-desktops envs
 
 while true; do
     rm /tmp/.X0-lock &>/dev/null || true
-    startx /usr/src/app/node_modules/electron-prebuilt/dist/electron /usr/src/app --enable-logging
+    startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
 done
