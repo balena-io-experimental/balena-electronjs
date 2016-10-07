8dd08598c97c370f9dea1e68f8263c7122823d55
curcuz <czkzcz.cmc@gmail.com>
2016-10-07 14:24:53 +0200

    added bower

--- app/start.sh
+++ app/start.sh
@@ -4,5 +4,6 @@
 # easily control specific version dependency between your app and electron itself.
 # the syntax below starts an X istance with ONLY our electronJS fired up,
 # it saves you a LOT of resources avoiding full-desktops envs
-    rm /tmp/.X0-lock &>/dev/null || true
-    startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
+
+rm /tmp/.X0-lock &>/dev/null || true
+startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
