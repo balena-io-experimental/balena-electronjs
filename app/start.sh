c29297b0e73a6aa31aebba5954a18ba241a8b27c
Levin Rickert <me@levrik.io>
2018-03-27 11:31:40 +0200

    Fix syntax error in start.sh

--- app/start.sh
+++ app/start.sh
@@ -11,7 +11,7 @@ umount /dev/shm && mount -t tmpfs shm /dev/shm
 
 rm /tmp/.X0-lock &>/dev/null || true
 
-if [ ! -c /dev/fb1 ] && [[ "$TFT" = "1" ]; then
+if [ ! -c /dev/fb1 ] && [ "$TFT" = "1" ]; then
   modprobe spi-bcm2708 || true
   modprobe fbtft_device name=pitft verbose=0 rotate=${TFT_ROTATE:-0} || true
   sleep 1
