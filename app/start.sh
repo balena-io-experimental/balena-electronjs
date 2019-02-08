057b9e598fdddd8cf8502e7bb49a51c041aed0a4
Jay Wilson <jwilson@varsity.com>
2019-02-08 14:50:29 -0600

    start fix for chromium

--- app/start.sh
+++ app/start.sh
@@ -10,13 +10,27 @@ umount /dev/shm && mount -t tmpfs shm /dev/shm
 # it saves you a LOT of resources avoiding full-desktops envs
 
 rm /tmp/.X0-lock &>/dev/null || true
+sed -i -e 's/console/anybody/g' /etc/X11/Xwrapper.config
 
-if [ ! -c /dev/fb1 ] && [ "$TFT" = "1" ]; then
-  modprobe spi-bcm2708 || true
-  modprobe fbtft_device name=pitft verbose=0 rotate=${TFT_ROTATE:-0} || true
-  sleep 1
-  mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ') || true
-  FRAMEBUFFER=/dev/fb1 startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
-else
-  startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
-fi
+
+
+#if [ ! -c /dev/fb1 ] && [ "$TFT" = "1" ]; then
+#  modprobe spi-bcm2708 || true
+#  modprobe fbtft_device name=pitft verbose=0 rotate=${TFT_ROTATE:-0} || true
+#  sleep 1
+#  mknod /dev/fb1 c $(cat /sys/class/graphics/fb1/dev | tr ':' ' ') || true
+#  FRAMEBUFFER=/dev/fb1 startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
+#else
+#  startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging
+#fi
+
+useradd test -m -s /bin/bash -G root
+usermod -a -G root,tty test
+echo "#!/bin/bash" > /home/test/xstart.sh
+echo "chromium-browser --kiosk $URL_LAUNCHER_URL" >> /home/test/xstart.sh
+chmod 770 /home/test/xstart.sh
+chown test:test /home/test/xstart.sh
+
+
+#su -c 'startx' test
+su -c 'startx /home/test/xstart.sh' test
